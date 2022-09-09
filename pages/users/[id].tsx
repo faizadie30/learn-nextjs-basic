9669dd6fa0b36be1d57fb5389db337a77f1b839e
faizadie30 <faiz.adie30@gmail.com>
2022-09-10 03:10:02 +0700

    learn get static paths

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,17 +1,56 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/Layout';
 
-export default function detail() {
-  const router = useRouter();
-  const { id } = router.query;
-  console.log('id:', id);
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+interface UserDetailProps {
+  user: User;
+}
+
+export default function UserDetail(props: UserDetailProps) {
+  const { user } = props;
+
   return (
     <Layout pageTitle="Detail User">
       <h1>Detail User Page</h1>
+
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <p>{user.phone}</p>
+      <p>{user.website}</p>
     </Layout>
   );
 }
 
 export async function getStaticPaths() {
-  
+  const response = await fetch('http://localhost:3004/users');
+  const dataUsers = await response.json();
+
+  const paths = dataUsers.map((user: User) => ({
+    params: {
+      id: `${user.id}`,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+interface GetStaticProps {
+  params: {
+    id: string;
+  };
+}
+export async function getStaticProps(context: GetStaticProps) {
+  const { id } = context.params;
+  const response = await fetch(`http://localhost:3004/users/${id}`);
+  const user = await response.json();
+
+  return { props: { user } };
 }
