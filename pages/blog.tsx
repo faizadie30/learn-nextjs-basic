94a9f569949a0359b62f0516ea399cf107179704
faizadie30 <faiz.adie30@gmail.com>
2022-09-10 22:02:02 +0700

    learn git serverside props

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,42 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
+import styles from '../styles/Blog.module.css';
 
-export default function blog() {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface BlogProps {
+  dataBlog: Post[];
+}
+
+export default function blog(props: BlogProps) {
+  const { dataBlog } = props;
+
   return (
     <Layout pageTitle="Blog">
-      <h1>Blog Page</h1>
+      <h1 className="title-homepage">Blog Page</h1>
+
+      {dataBlog.map((blog, key) => {
+        return (
+          <div key={key} className={styles.card}>
+            <h3>{blog.title}</h3>
+            <p>{blog.body}</p>
+          </div>
+        );
+      })}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3004/blog');
+  const dataBlog = await res.json();
+
+  return {
+    props: {
+      dataBlog,
+    },
+  };
 }
